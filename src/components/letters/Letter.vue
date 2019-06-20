5750a3861a6f0e0ebd1e20e538ad7ee6441b5446
aFrontendDev <grumpydev22@gmail.com>
2019-06-20 21:53:21 +0100

    Replace multiple lines with templated lines

--- src/components/letters/Letter.vue
+++ src/components/letters/Letter.vue
@@ -1,13 +1,6 @@
 <template>
   <div :class="className(letter)">
-    <div v-for="n in 10" v-bind:key="makeKey(n, 'a1')" :class="classCount(n, 'a1')"></div>
-    <div v-for="n in 10" v-bind:key="makeKey(n, 'a2')" :class="classCount(n, 'a2')"></div>
-    <div v-for="n in 10" v-bind:key="makeKey(n, 'a3')" :class="classCount(n, 'a3')"></div>
-    <div v-for="n in 10" v-bind:key="makeKey(n, 'a4')" :class="classCount(n, 'a4')"></div>
-    <div v-for="n in 10" v-bind:key="makeKey(n, 'a5')" :class="classCount(n, 'a5')"></div>
-    <div v-for="n in 10" v-bind:key="makeKey(n, 'a6')" :class="classCount(n, 'a6')"></div>
-    <div v-for="n in 10" v-bind:key="makeKey(n, 'a7')" :class="classCount(n, 'a7')"></div>
-    <div v-for="n in 10" v-bind:key="makeKey(n, 'a8')" :class="classCount(n, 'a8')"></div>
+    <div v-for="n in 80" v-bind:key="makeKey(n)" :class="makeKey(n)"></div>
   </div>
 </template>
 
@@ -16,11 +9,14 @@ export default {
   name: "Letter",
   props: ["letter"],
   methods: {
-    makeKey: (count, string) => {
-      return `${string}-${count}`;
-    },
-    classCount: (count, string) => {
-      return `${string}-${count}`;
+    makeKey: count => {
+      const num = count;
+      const stringA = `a${Math.ceil(num / 10)}`;
+      const aTen = (num / 10) % 1 === 0;
+      const dec = ((num / 10) % 1).toFixed(1).split(".")[1];
+      const endNum = aTen ? 10 : dec;
+      const stringB = `-${endNum}`;
+      return `block ${stringA}${stringB}`;
     },
     className: letter => {
       return `letter letter--${letter}`;
