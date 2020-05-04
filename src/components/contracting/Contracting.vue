05389390c9bd0ca2b48e7319e1afde01d67aca33
Andy Blackledge <blackledge22@gmail.com>
2020-05-04 10:02:17 +0100

    Content change

--- src/components/contracting/Contracting.vue
+++ src/components/contracting/Contracting.vue
@@ -7,7 +7,7 @@
     </header>
     <div class="section__body">
       <p>
-        I provide my services as a front end developer to work for companies over a limited period at a day rate we can negotiate. This can be for specific projects or to augment your existing development team if you find yourself temporarily short on resources.
+        I provide my services as a front end developer to work for companies over a specified period on the agreed upon project at a day rate we can negotiate.
       </p>
       <p>
         I'm happy to work on-site with your company providing it's in or around Manchester, alternatively I'm quite happy to work remotely and can join standups/meetings/etc via skype. I will also be available for a mixture of remote and on-site if we come to an agreement on a ratio that suits both parties. I generally try to be as flexible and accommodating as I can be.
